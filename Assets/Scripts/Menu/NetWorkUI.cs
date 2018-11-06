0c99cbf9c3ce569198d188d5a633f59bb3ebd338
Anis Mezdari <mezdari.anis@gmail.com>
2018-11-06 22:19:55 +0100

    scene host/client/exit

---
+++ Assets/Scripts/Menu/NetWorkUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetWorkUI : NetworkManager {
+
+   
+
+    public void StartupHost()
+    {
+        Debug.Log("tessstt");
+        SetPort();   
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinGame()
+    {
+        SetIPAddress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
+   
+    }
+    public void SetIPAddress()
+    {
+        string ipAddress = GameObject.Find("fieldText").transform.GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAddress;
+    }
+   void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 24580;
+    }
+}
---
+++ Assets/Scripts/Menu/eventMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class eventMenu : MonoBehaviour {
+
+    public NetWorkUI nwu;
+    public Button host;
+    public Button client;
+
+    // Use this for initialization
+    void Start()
+    {
+        host.onClick.AddListener(delegate { nwu.StartupHost(); });
+        client.onClick.AddListener(delegate{nwu.JoinGame(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
