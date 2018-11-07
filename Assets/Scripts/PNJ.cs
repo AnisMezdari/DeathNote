bc4dfda4d7e0a8d1849da0de347c9cb59d55d403
Anis Mezdari <mezdari.anis@gmail.com>
2018-11-07 19:30:05 +0100

    PNJ pathFinding

--- Assets/Scripts/PNJ.cs
+++ Assets/Scripts/PNJ.cs
@@ -1,5 +1,11 @@
-﻿
+﻿using UnityEngine.AI;
+using UnityEngine;
+
 public class PNJ : Character {
+
+
+    public NavMeshAgent agent;
+    public Transform target;
 
     public PNJ(bool isInteractable, string name) :
         base(isInteractable, name) {}
@@ -12,5 +18,16 @@ public class PNJ : Character {
     public override void SetIsInteractable()
     {
         
+    }
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;    
+    }
+
+    void Update()
+    {
+
     }
 }
