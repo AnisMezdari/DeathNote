cdadb87d095de16419f0a1770c95ac6a1a56e8bf
Anis Mezdari <mezdari.anis@gmail.com>
2018-11-08 23:20:51 +0100

    Instanciation + NameRandom + pathfinding

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.AI;
+using System.IO;
 
-public class Game : MonoBehaviour {
+public class Game : NetworkBehaviour
+{
 
     private double notoriety;
     private List<PNJ> pnjList;
+    public const int NOMBRE_PNJ = 30;
+    public const string PATH_FILE_PNJ_NAMES = "Assets/Resources/PNJ/pnj_names.txt";
 
     public Game()
     {
         this.notoriety = 0;
-        this.pnjList = new List<PNJ>();
+        this.pnjList = new List<PNJ>(NOMBRE_PNJ);
     }
 
     public void DesignateStudentToDieList()
@@ -24,11 +30,43 @@ public class Game : MonoBehaviour {
     {
 
     }
+    public List<string> LoadPNJnameFile()
+    {
+        FileInfo theSourceFile = null;
+        StreamReader reader = null;
+        string text = " "; // assigned to allow first line to be read below
+
+        theSourceFile = new FileInfo(PATH_FILE_PNJ_NAMES);
+        reader = theSourceFile.OpenText();
+
+        List<string> listNamePNJ = new List<string>(NOMBRE_PNJ);
+
+        int n = 0;
+        while (n < NOMBRE_PNJ - 1)
+        {
+            text = reader.ReadLine();
+            listNamePNJ.Add(text);
+            n++;
+        }
+        reader.Close();
+
+        return listNamePNJ;
+    }
     public void LoadPNJ()
     {
-
+        if (isServer)
+        {
+            List<string> listNamePNJ = LoadPNJnameFile();
+            foreach (var name in listNamePNJ)
+            {
+                int randomNumber = Random.Range(1, 10 - 1);
+                PNJ pnj = new PNJ(true, name, Resources.Load("PNJ/PNJ"), GameObject.Find("Cube"+ randomNumber).transform);
+                GameObject gameObjectPnj = pnj.Instantiate(GameObject.Find("Cube"+ randomNumber).transform.position);
+                NetworkServer.Spawn(gameObjectPnj);
+            }
+        }     
     }
-    public void loadPlayer()
+    public void LoadPlayer()
     {
 
     }
@@ -38,10 +76,15 @@ public class Game : MonoBehaviour {
     }
     public void StartGame()
     {
-
+        
     }
     public void EndGame()
     {
 
+    }
+
+    void Start()
+    {
+        LoadPNJ();
     }
 }
--- Assets/Scripts/PNJ.cs
+++ Assets/Scripts/PNJ.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine.AI;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PNJ : Character {
-
 
     public NavMeshAgent agent;
     public Transform target;
+    private Object prefab;
+    private const float DISTANCE_TARGET_BC = 3.0f;
 
-    public PNJ(bool isInteractable, string name) :
-        base(isInteractable, name) {}
+    public PNJ(bool isInteractable, string name, Object prefab, Transform target)
+        : base(isInteractable, name)
+    {
+        this.prefab = prefab;
+        this.target = target;
+    }
+
+    public GameObject Instantiate(Vector3 initialPosition)
+    {
+        GameObject gameObjectPnj =  Instantiate(prefab, initialPosition, Quaternion.identity) as GameObject;
+        gameObjectPnj.GetComponent<PNJ>().target = this.target;
+        gameObjectPnj.GetComponent<PNJ>().isInteractable = this.isInteractable;
+        gameObjectPnj.GetComponent<PNJ>().name = this.name;
+
+        return gameObjectPnj;
+    }
 
     public override void Moving()
     {
@@ -19,15 +37,34 @@ public class PNJ : Character {
     {
         
     }
+    public void SetTarget(Transform target )
+    {
+        this.target = target;
+    }
+
+    public bool HasSamePositionOfTarget()
+    {
+        return Vector3.Distance(this.transform.position, target.position) < DISTANCE_TARGET_BC;
+    }
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        agent.destination = target.position;    
+        // PathFinfing, permit to initiate the target of moving
+        this.agent = GetComponent<NavMeshAgent>();
+        this.agent.destination = this.target.position;
+        this.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = this.name ;   
     }
 
     void Update()
     {
 
+        if (HasSamePositionOfTarget())
+        {
+            transform.Translate(0, 0, 0);
+            int randomNumber = Random.Range(1, 10 - 1);
+            target = GameObject.Find("Cube" + randomNumber).transform;
+            agent.destination = target.position;
+        }
+        
     }
 }
